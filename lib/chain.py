2ecdec6cb3a4c20b9d80054cddd4a70c110faeb8
0nix <boundlessimagination@gmail.com>
2016-01-18 14:51:11 -0600

    YASM

--- lib/chain.py
+++ lib/chain.py
@@ -2,5 +2,5 @@ from jsonrpc import ServiceProxy
 #client = ServiceProxy("http://jenn:password@127.0.0.1:2776")
 def getNewWallet():
 	client = ServiceProxy("http://jenn:password@127.0.0.1:2776")
-	newWallet = client.getNewAddress()
+	newWallet = client.getnewaddress()
 	return newWallet
