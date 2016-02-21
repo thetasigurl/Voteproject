1ceec0c2fa54aa08a25f3306682a58bda81ef0eb
0nix <boundlessimagination@gmail.com>
2016-02-21 11:51:52 -0600

    Added student csv

--- lib/rpc.py
+++ lib/rpc.py
@@ -3,4 +3,4 @@ import chain
 parser = argparse.ArgumentParser()
 connstr = "http://jenn:password@127.0.0.1:2776"
 #print chain.getNewWallet(connstr)
-print chain.getVotesFromWallet(connstr,"1A4wnUwbeGrQFfj9pQ9SCUgqAqj7xKfZ6vE1VW")
+#print chain.getVotesFromWallet(connstr,"1A4wnUwbeGrQFfj9pQ9SCUgqAqj7xKfZ6vE1VW")
