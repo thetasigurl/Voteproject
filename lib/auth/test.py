689fb1144f6bf315b02aad13b4699ee4edd27662
0nix <boundlessimagination@gmail.com>
2016-03-21 23:23:03 -0500

    WRONG METHOD NAME

--- lib/auth/test.py
+++ lib/auth/test.py
@@ -1,5 +1,5 @@
 import db
 
 def main():
-	print(db.hash("01b8e80ed0d7172d6728339f5e634226ed12ebfb60b0e7bfb309541cddf4906b"))
+	print(db.hashed("01b8e80ed0d7172d6728339f5e634226ed12ebfb60b0e7bfb309541cddf4906b"))
 main()
