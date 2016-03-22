597703aebdede83ff092b7a53b83f17f184ac956
0nix <boundlessimagination@gmail.com>
2016-03-21 23:10:14 -0500

    OH GOD WHOOPS

--- lib/auth/server.py
+++ lib/auth/server.py
@@ -2,24 +2,6 @@ from flask import Flask
 import flask
 import db
 app = Flask(__name__)
-class InvalidUsage(Exception):
-    status_code = 400
-    def __init__(self, message, status_code=None, payload=None):
-        Exception.__init__(self)
-        self.message = message
-        if status_code is not None:
-            self.status_code = status_code
-        self.payload = payload
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv['message'] = self.message
-        return rv
-@app.errorhandler(InvalidAPIUsage)
-def handle_invalid_usage(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
-
 @app.route("/")
 def hello():
 	return "Hello this is Flask Auth!"
