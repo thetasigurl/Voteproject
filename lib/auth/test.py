e2200f72638b211d839b2c560af24c17126e2dd3
0nix <boundlessimagination@gmail.com>
2016-03-21 23:51:57 -0500

    very angry

--- lib/auth/test.py
+++ lib/auth/test.py
@@ -1,5 +1,7 @@
 import db
 
 def main():
-	print(db.hashed("01b8e80ed0d7172d6728339f5e634226ed12ebfb60b0e7bfb309541cddf4906b"))
+	r = db.hashed("01b8e80ed0d7172d6728339f5e634226ed12ebfb60b0e7bfb309541cddf4906b")
+	print(r)
+	print(type(r))
 main()
