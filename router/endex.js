742c5b4d29f9a02ada872e1920f844fb7b3efc0b
onix <g.eduardodeluna@gmail.com>
2017-03-23 16:38:07 -0500

    trying the router

--- router/endex.js
+++ router/endex.js
@@ -16,7 +16,7 @@ app.set('port',process.env.PORT || 5000);
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use('/',[require('./routes.js')]);
+app.use('/',[require('./routes')]);
 http.createServer(app).listen(app.get('port'), () => {
 	console.log("Server started in port: ", app.get('port'))
 });
